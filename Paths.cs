933e90dfc7096d76ce60df7e050cf8f86936ca01
Jobus <johan.tylon@gmail.com>
2024-01-26 17:43:13 +0100

    Rename add-on supported lib folder to 'ReShade-AddonSupport'

--- Paths.cs
+++ Paths.cs
@@ -12,7 +12,7 @@ public static class Paths
     public static readonly string Shaders = Path.Combine(Root, "Shaders");
     public static readonly string Textures = Path.Combine(Root, "Textures");
     public static readonly string Dlls = Path.Combine(Root, "lib", "ReShade");
-    public static readonly string AddonDlls = Path.Combine(Root, "lib", "ReShade-Addons");
+    public static readonly string AddonDlls = Path.Combine(Root, "lib", "ReShade-AddonSupport");
     public static readonly string Cache = Path.Combine(Root, "lib", "Cache");
     public static readonly string ReShadeIni = Path.Combine(Root, "ReShade.ini");
     public static readonly string ReShadePresetIni = Path.Combine(Root, "ReShadePreset.ini");
