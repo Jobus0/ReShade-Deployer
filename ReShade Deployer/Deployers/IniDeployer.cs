45f1bad7ac0a9b7ada49bb86b1ae76ef3d94f498
Jobus <johan.tylon@gmail.com>
2025-08-23 19:18:51 +0200

    Small refactor of IniDeployer

--- ReShade Deployer/Deployers/IniDeployer.cs
+++ ReShade Deployer/Deployers/IniDeployer.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using IniParser;
 using IniParser.Model;
 
 namespace ReShadeDeployer;
@@ -16,24 +17,31 @@ public static class IniDeployer
         if (File.Exists(path))
             File.Delete(path);
         
-        IniParser.FileIniDataParser iniParser = new();
+        var iniParser = new FileIniDataParser();
         iniParser.Parser.Configuration.AssigmentSpacer = "";
+        
         IniData ini = File.Exists(Paths.ReShadeIni)
             ? iniParser.ReadFile(Paths.ReShadeIni)
             : new IniData();
 
+        ApplyContextualDefaults(ini, executableContext);
+        
+        iniParser.WriteFile(path, ini);
+    }
+    
+    private static void ApplyContextualDefaults(IniData ini, ExecutableContext executableContext)
+    {
         ini["GENERAL"]["EffectSearchPaths"] = Paths.Shaders + "\\**";
         ini["GENERAL"]["TextureSearchPaths"] = Paths.Textures + "\\**";
         ini["GENERAL"]["IntermediateCachePath"] = Paths.Cache;
         ini["GENERAL"]["PresetPath"] = @".\ReShadePreset.ini";
-        ini["GENERAL"]["PreprocessorDefinitions"] = $"RESHADE_DEPTH_LINEARIZATION_FAR_PLANE=1000.0," +
-                                                    $"RESHADE_DEPTH_INPUT_IS_UPSIDE_DOWN={executableContext.DepthUpsideDown}," +
-                                                    $"RESHADE_DEPTH_INPUT_IS_REVERSED={executableContext.DepthReversed}," +
-                                                    $"RESHADE_DEPTH_INPUT_IS_LOGARITHMIC={executableContext.DepthLogarithmic}";
+        ini["GENERAL"]["PreprocessorDefinitions"] =
+            $"RESHADE_DEPTH_LINEARIZATION_FAR_PLANE=1000.0," +
+            $"RESHADE_DEPTH_INPUT_IS_UPSIDE_DOWN={executableContext.DepthUpsideDown}," +
+            $"RESHADE_DEPTH_INPUT_IS_REVERSED={executableContext.DepthReversed}," +
+            $"RESHADE_DEPTH_INPUT_IS_LOGARITHMIC={executableContext.DepthLogarithmic}";
         ini["OVERLAY"]["TutorialProgress"] = "4";
         ini["DEPTH"]["DepthCopyBeforeClears"] = executableContext.DepthCopyBeforeClears;
         ini["DEPTH"]["UseAspectRatioHeuristics"] = executableContext.UseAspectRatioHeuristics;
-        
-        iniParser.WriteFile(path, ini);
     }
 }
